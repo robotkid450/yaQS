70b3478e2def66a08cd9830dfbc36c926bfa8a77
Josh Smith <robotkid450@gmail.com>
2016-07-14 18:09:46 -0500

    moved messageProtocol to its own file

--- server.py
+++ server.py
@@ -3,91 +3,48 @@ import socketserver
 import queue
 import json
 import sys
-import struct
-
-'''class MessageProtocol(object):
-    """docstring for MessageProtocol"""
-    def __init__(self, socket):
-        self.socket = socket'''
+import MessageProtocol
 
 
 class TcpHandler(socketserver.BaseRequestHandler):
 
-    '''def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024)
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())'''
-    # The following functions define a binary protocol used to communicate
-    # between the server and its clients
-
-    # A message consists of a 4byte block containing the length of the message
-    # followed by the meassage contents
-
-    def send_message(self, sock, msg):
-        # Prefix each message with a 4-byte length (network byte order)
-        msg = struct.pack('>I', len(msg)) + msg.encode()
-        sock.sendall(msg)
-
-    def recv_message(self, sock):
-        # Read message length and unpack it into an integer
-        raw_msglen = self.recvall(sock, 4)
-        if not raw_msglen:
-            return None
-        msglen = struct.unpack('>I', raw_msglen)[0]
-        # Read the message data
-        return self.recvall(sock, msglen).decode()
-
-    def recvall(self, sock, n):
-        # Helper function to recv n bytes or return None if EOF is hit
-        data = b''
-        while len(data) < n:
-            packet = sock.recv(n - len(data))
-            if not packet:
-                return None
-            data += packet
-        return data
-
-    # End protocal functions
-
     def setup(self):
         self.que = q
+        self.MP = MessageProtocol.Message(self.request)
 
     def handle(self):
-        self.data = self.recv_message(self.request)
+        self.data = self.MP.recv_message()
         if self.data == 'addJob': #adds jobs to queue
             print('adding Job')
-            self.send_message(self.request, 'send job')
-            self.job_to_add = json.loads(self.recv_message(self.request))
+            self.MP.send_message('send job')
+            self.job_to_add = json.loads(self.MP.recv_message())
             self.que.addJob(self.job_to_add[0],self.job_to_add[1], self.job_to_add[2])
             print('added job :', self.job_to_add[0])
 
         elif self.data == 'getAllJobs':
             print('getting jobs')
             self.all_jobs = self.que.getAllJobs()
-            self.send_message(self.request, json.dumps(self.all_jobs))
+            self.MP.send_message(json.dumps(self.all_jobs))
 
         elif self.data == 'getJobInfo':
-            self.send_message(self.request, 'send ID')
-            self.job_ID = self.recv_message(self.request)
+            self.MP.send_message('send ID')
+            self.job_ID = self.MP.recv_message()
             self.job_Info = self.que.getJobInfo(self.job_ID)
-            self.send_message(self.request, json.dumps(self.job_Info))
+            self.MP.send_message(json.dumps(self.job_Info))
 
         elif self.data == 'removeJob':
-            self.send_message(self.request, 'send ID')
-            self.job_ID = self.recv_message(self.request)
+            self.MP.send_message('send ID')
+            self.job_ID = self.MP.recv_message()
             self.result = self.que.removeJob(self.job_ID)
-            self.send_message(self.request, str(self.result))
+            self.MP.send_message(str(self.result))
 
         else:
-            self.send_message(self.request, 'invalid command')
+            self.MP.send_message('invalid command')
 
 
     def finish(self):
         q = self.que
-        self.send_message(self.request, 'done')
+        self.MP.send_message('done')
 
 if __name__ == "__main__":
     try:
