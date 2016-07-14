aca541e45685e2eb339ce75f511ab58b2c1a8345
Josh Smith <robotkid450@gmail.com>
2016-07-14 18:55:59 -0500

    added getJobToRun getJobToRun: retrives next job based on input order and priority or returns -1 if no jobs are avalible

--- queue.py
+++ queue.py
@@ -102,6 +102,23 @@ class QueueData(object):
     def modJob(self, jobID, job, command, priority): # will be implemted later
         return 0
 
+    def getJobToRun(self):
+        # retrives next job based on input order and priority or
+        # returns -1 if no jobs are avalible
+        if len(self.HPque) != 0:
+            job_to_run = self.HPque.pop()
+
+        elif len(self.SPque) != 0:
+            job_to_run = self.SPque.pop()
+
+        elif len(self.LPque) != 0:
+            job_to_run = self.LPque.pop()
+
+        else:
+            return -1
+
+        return job_to_run
+
 
 if __name__ == '__main__':
     # test code
