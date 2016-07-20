9915d3bf2d49f47fb6baac3bc14f4ec5dc2ce216
Josh Smith <robotkid450@gmail.com>
2016-07-19 20:10:17 -0500

    added needed global variables + helper functions

--- cli.py
+++ cli.py
@@ -2,4 +2,24 @@
 
 import socket
 import json
-import argprase
+import argparse
+
+# Define needed global variables
+host = 'localhost'
+tcpPort = 9998
+udpPort = 9999
+
+# Define socket
+sock = socket.socket()
+
+# Helper functions
+def send_message(sock, command, data=''):
+        data_to_encode = (command, data)
+        data_to_send = json.dumps(data_to_encode)
+        sock.send(data_to_send.encode())
+
+def recv_message(sock):
+    data_to_decode = sock.recv(1024).decode()
+    command, data = json.loads(data_to_decode)
+    return command, data
+# End helper function
