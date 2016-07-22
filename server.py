ae8227a8d6f9dcf1ad9598daa9df05e68ebe3ec6
Josh Smith <robotkid450@gmail.com>
2016-07-21 23:30:37 -0500

    added comments

--- server.py
+++ server.py
@@ -15,10 +15,10 @@ server_addr = ('127.0.0.1', 9998) # testing
 # broadcast_addr = ('255.255.255.255', 9999) # production
 broadcast_addr = ('127.0.0.1', 9999) # testing
 
-discovery_intreval = 10
-workDispatch_intreval = 1
+discovery_intreval = 10   # time in seconds between discovery broadcasrs
+workDispatch_intreval = 1 # time in seconds between workDispatch broadcasts
 
-class UDPBroadcaster(object):
+class UDPBroadcaster(object): # UDP broadcaster class
     """docstring for UDPBroadcaster"""
     def __init__(self):
         # SOCK_DGRAM is the socket type to use for UDP sockets
@@ -26,31 +26,32 @@ class UDPBroadcaster(object):
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-    def sendDiscovery(self):
+    def sendDiscovery(self): # sends discovery broadcast
         self.sock.sendto('discover'.encode(), broadcast_addr)
         # print('sending discover')
         self._stop()
 
-    def sendWorkAvailable(self):
+    def sendWorkAvailable(self): # sends work avalible broadcast
         self.sock.sendto('work Available'.encode(), broadcast_addr)
         # print('sending work avalible')
         self._stop()
 
-    def _stop(self):
+    def _stop(self): # stops broadcast repeat loop
         self.sock.close()
 
-def workDispatch():
+def workDispatch(): # helper function for workDispatch broadcast
     if queue.getJobsAvailable() > 0:
         UB = UDPBroadcaster()
         UB.sendWorkAvailable()
     return 0
 
-def discovery():
+def discovery(): # helper function for discovery broadcast
     UB = UDPBroadcaster()
     UB.sendDiscovery()
     return 0
 
 
+# async Protocol object for data server
 class dataServerProtocol(asyncio.Protocol):
     """docstring for MessageProtocol"""
     def connection_made(self, transport):
@@ -72,61 +73,60 @@ class dataServerProtocol(asyncio.Protocol):
             print('added job :', job_to_add[0])
             self.send_message(data=result)
 
-        elif command == 'getAllJobs':
+        elif command == 'getAllJobs': # gets all jobs currently in system
             print('getting jobs')
             all_jobs = self.que.getAllJobs()
             self.send_message(data=all_jobs)
 
-        elif command == 'getJobInfo':
+        elif command == 'getJobInfo': # get all information on a job
             print('job info request')
             job_ID = cmd_data
             job_Info = self.que.getJobInfo(job_ID)
             self.send_message(data=job_Info)
 
-        elif command == 'removeJob':
-            print('job remove request')
-            job_ID= cmd_data
+        elif command == 'removeJob': # removes a job from queue DOES NOT STOP
+            print('job remove request') # RUNNING JOB!!
+            job_ID = cmd_data
             print('removing job:', job_ID)
             result = self.que.removeJob(job_ID)
             self.send_message(data=result)
 
-        elif command == 'getJobToRun':
+        elif command == 'getJobToRun': # gets next job from queue
             print('Run job request')
             job_to_run = self.que.getJobToRun()
             print('sending job: ', job_to_run)
             self.send_message(data=job_to_run)
 
-        elif command == 'submitJobComplete':
+        elif command == 'submitJobComplete': # removes job from running list
             print('submitJobComplete request')
             self.que.markRunningJobComplete(cmd_data[0])
 
-        elif command == 'shutdown':
+        elif command == 'shutdown': # remotely kills server
             self.transport.close()
             self.quitter()
 
         else:
-            print('invalid command')
+            print('invalid command') # replies to invalid commands
             self.send_message(data='invalid command')
 
         self.transport.close()
 
-    def connection_lost(self, exc):
+    def connection_lost(self, exc): # executed on connection loss
         print('client disconnected')
         queue = self.que
 
 
     # helper functions
-    def send_message(self, command='reply', data=''):
+    def send_message(self, command='reply', data=''):# composes & sends messages
             data_to_encode = (command, data)
             data_to_send = json.dumps(data_to_encode)
             self.transport.write(data_to_send.encode())
 
-    def quitter(self):
-        #used to remotly kill server
+    def quitter(self): # helper funcyion for shutdown command
         loop.stop()
 
 
-class PeriodicTask(object):
+class PeriodicTask(object): # base for tasks that run periodicly ex. broadcasts
     def __init__(self, func, interval):
         self.func = func
         print(func)
@@ -150,12 +150,15 @@ if __name__ == '__main__':
 
     # Create asyncio loop
     loop = asyncio.get_event_loop()
-    # Each client connection will create a new protocol instance
+    # Create data Server Coroutine
     dataServerCoroutine = loop.create_server(
         dataServerProtocol, server_addr[0], server_addr[1])
+    # run data Server Coroutine
     dataServer = loop.run_until_complete(dataServerCoroutine)
+    # Create and run broadcasts
     dispatchServer = PeriodicTask(workDispatch, workDispatch_intreval)
     discoverServer = PeriodicTask(discovery, discovery_intreval)
+    # Send initial discovery broadcast
     discovery()
 
     # Serve requests until Ctrl+C is pressed
