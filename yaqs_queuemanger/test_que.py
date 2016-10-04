234f5d8b1d6260d917386e96be247b14835722a5
Josh Smith <robotkid450@gmail.com>
2016-10-04 14:52:01 +0000

    added tests for getInfo

--- yaqs_queuemanger/test_que.py
+++ yaqs_queuemanger/test_que.py
@@ -5,6 +5,12 @@ from sys import exit
 q = yaqsQueue.QueueData()  #create que obj
 
 q.addJob('HPtest', 'command', 1, 'workingDirectory')
-jID= q.HPque[0].id
-print(q.getJobInfo(jID))
+q.addJob('HPtest', 'command', 2, 'workingDirectory')
+q.addJob('HPtest', 'command', 3, 'workingDirectory')
+hpID= q.HPque[0].id
+spID= q.SPque[0].id
+lpID= q.LPque[0].id
+print('hp : ', q.getJobInfo(hpID))
+print('sp : ', q.getJobInfo(spID))
+print('lp ; ', q.getJobInfo(lpID))
 
