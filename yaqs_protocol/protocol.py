53a2f7daceab8afb45273544e1b9f91169a2a85c
Josh Smith <robotkid450@gmail.com>
2016-11-09 19:35:54 -0600

    continued work on Protocol.py

--- yaqs_protocol/protocol.py
+++ yaqs_protocol/protocol.py
@@ -3,39 +3,46 @@ __version__ = '2.3.2'
 
 
 class Protocol:
-    def _send_msg(sock, msg):
+
+    def __init__(self, sock):
+        self.sock= sock
+
+    def _send_msg(self, msg):
         # Prefix each message with a 4-byte length (network byte order)
         msg = struct.pack('>I', len(msg)) + msg
-        sock.sendall(msg)
+        self.sock.sendall(msg)
 
-    def _recv_msg(sock):
+    def _recv_msg(self):
         # Read message length and unpack it into an integer
-        raw_msglen = recvall(sock, 4)
+        raw_msglen = recvall(4)
         if not raw_msglen:
             return None
         msglen = struct.unpack('>I', raw_msglen)[0]
         # Read the message data
-        return recvall(sock, msglen)
+        return recvall(msglen)
 
-    def _recvall(sock, n):
+    def _recvall(self, n):
         # Helper function to recv n bytes or return None if EOF is hit
         data = ''
         while len(data) < n:
-            packet = sock.recv(n - len(data))
+            packet = self.sock.recv(n - len(data))
             if not packet:
                 return None
             data += packet
         return data
 
 
-
-    def sendMessage(sock, command, data=''): # composes & sends messages
+    #end helper functions
+    def sendMessage(self, command, data=''): # composes & sends messages
         data_to_json = (command, data)
         data_to_send = json.dumps(data_to_encode)
-        _send_msg(sock, data_to_send.encode())
-        #sock.send(data_to_send.encode())
+        try:
+            _send_msg(self, data_to_send.encode())
+            return 0
+        except:
+            return -1
 
-    def recvMessage(sock): # recives and decomposes messages
-        data_to_decode = _recv_msg(sock)
+    def recvMessage(self): # recives and decomposes messages
+        data_to_decode = _recv_msg()
         command, data = json.loads(data_to_decode)
         return command, data
