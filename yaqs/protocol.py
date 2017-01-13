6daaff4d085055fcbdf84be9aa4d68708fbf743b
Josh Smith <robotkid450@gmail.com>
2017-01-13 11:58:21 -0600

    moving to differnt computer

--- yaqs/protocol.py
+++ yaqs/protocol.py
@@ -8,20 +8,25 @@ import json
 class Client:
 
     def __init__(self, sock):
-        self.sock= sock
+        self.sock = sock
 
     def _send_msg(self, msg):
         # Prefix each message with a 4-byte length (network byte order)
+        print("_send_msg msg len", len(msg))
         msg = struct.pack('>I', len(msg)) + msg
+        msglen = struct.unpack('>I', msg[:4])#[0]
+        print("_send_msg msg raw len", msglen)
         self.sock.sendall(msg)
 
     def _recv_msg(self):
         # Read message length and unpack it into an integer
         raw_msglen = self._recvall(4)
+        print("raw msg len", raw_msglen)
         if not raw_msglen:
             return None
         msglen = struct.unpack('>I', raw_msglen)[0]
         # Read the message data
+        print(" msg len", msglen)
         return self._recvall(msglen)
 
     def _recvall(self, n):
@@ -60,9 +65,12 @@ class Server:
     def _recv_msg(self, packed_message):
         # Read message length and unpack it into an integer
         msglen = struct.unpack('>I', packed_message[:4])[0]
+        print("server msglenn recev", msglen)
         if not msglen:
             return None
         msg = packed_message[4:msglen+4]
+        #print("msg payload follows")
+        #print(msg)
         return msg
 
     def sendMessage(self, command='reply', data=''): # composes & sends messages
